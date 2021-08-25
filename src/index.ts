ee2cc9c3f2f4ac2d4339dcba493c837b7e09f3e7
Azorimor <azorimor@gmail.com>
2021-08-25 16:03:58 +0200

    Added i18n with i18next and en/de lng sample

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,33 @@
-import { PORT } from "./utils/config";
+import { PORT, DEFAULT_LANGUAGE, PROD } from "./utils/config";
 import app from "./app";
 import logger from "./utils/logger";
 import connection from "./utils/connection";
+import i18next from "i18next";
+import en from "../locales/en.json";
+import de from "../locales/de.json";
 
 connection
   .then(() => {
     logger.info("Connected to MongoDB");
+    i18next.init(
+      {
+        lng: DEFAULT_LANGUAGE,
+        debug: !PROD,
+        initImmediate: false,
+        fallbackLng: "en",
+        resources: {
+          en,
+          de,
+        },
+      },
+      (error) => {
+        logger.info({
+          message: `Language loaded (${DEFAULT_LANGUAGE}).`,
+          metadata: { error: error, language: DEFAULT_LANGUAGE },
+        });
+      }
+    );
+
     app.listen(PORT, () => {
       logger.info(`Express server running on port ${PORT}`);
     });
