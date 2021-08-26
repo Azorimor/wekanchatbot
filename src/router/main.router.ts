c50beb09f40ad0d8a30262bcfa801e423290913f
Azorimor <azorimor@gmail.com>
2021-08-26 12:04:12 +0200

    removed debug log

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import wekanRouter from "./wekan.router";
 import i18next from "i18next";
-import logger from "../utils/logger";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  logger.info({ message: "testlang GET / :", metadata: i18next });
   res.json({ message: i18next.t("status"), success: true });
 });
 
